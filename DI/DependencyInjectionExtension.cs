82d7e615f5617b383bfef382d3ef66e00471aa30
guilherme.guia <guilhermeguia007@gmail.com>
2025-05-14 07:09:24 -0400

    add depedency injection

---
+++ DI/DependencyInjectionExtension.cs
@@ -0,0 +1,11 @@
+﻿using Autenticador.Services.Auth;
+
+namespace Autenticador.DI;
+
+public static class DependencyInjectionExtension
+{
+    public static void AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IAuthService, AuthService>();
+    }
+}
---
+++ Services/Auth/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Autenticador.Models.Request;
+
+namespace Autenticador.Services.Auth;
+
+public interface IAuthService
+{
+    Task<string> ExecuteRegister(RegisterRequest request);
+    //void ExecuteLogin();
+    //void ExecuteVerifyEmail();
+    //void ExecuteResetPassword();
+    //void ExecuteNewPassword();
+}
