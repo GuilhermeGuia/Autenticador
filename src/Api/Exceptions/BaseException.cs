07ac3df26ef28fab847469a8baea066d873572a3
Guilherme da Silva Guia <guilhermeguia007@gmail.com>
2025-05-22 17:48:57 -0400

    Alter exception project

--- src/Api/Exceptions/BaseException.cs
+++ src/Api/Exceptions/BaseException.cs
@@ -5,5 +5,5 @@ public abstract class BaseException : SystemException
 {
     public BaseException(string message) : base(message) { }
     public abstract HttpStatusCode GetStatusCode();
-    public abstract IList<string> GetErrorMessages();
+    public abstract string GetErrorMessage();
 }
---
+++ src/Api/Exceptions/RegisterException.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace Api.Exceptions;
+
+public class RegisterException(string message) : BaseException(message)
+{
+    public override string GetErrorMessage() => Message;
+    public override HttpStatusCode GetStatusCode() => HttpStatusCode.BadRequest;
+}
