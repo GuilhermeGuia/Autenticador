4f702243ab046407888e670ace7a81f02b7ab5e0
Guilherme da Silva Guia <guilhermeguia007@gmail.com>
2025-05-23 08:13:36 -0400

    Add filter in program

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,3 +1,6 @@
+using Api.DI;
+using Api.Filters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer(); // necessário para Swagger funcionar
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(typeof(ExceptionGlobalFilter));
+});
+
+builder.Services.AddServices();
+
 var app = builder.Build();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Autenticador v1");
+    c.RoutePrefix = string.Empty;
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Docker"))
