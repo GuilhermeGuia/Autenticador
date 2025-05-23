c5dbbfd27bce23aa735cba162aea5d6ef8a278a8
Guilherme da Silva Guia <guilhermeguia007@gmail.com>
2025-05-23 08:12:45 -0400

    Add base response

---
+++ src/Api/Models/Request/BaseRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace Api.Models.Request;
+
+public class BaseRequest<T>
+{
+    public string Message { get; set; }
+    public T? Data { get; set; }
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+    public BaseRequest(string message)
+    {
+        Message = message;
+    }
+    public BaseRequest(string message, T? data)
+    {
+        Message = message;
+        Data = data;
+    }
+}
