1fc88625b55206c139337a049bb5f662b0b3ce39
Guilherme da Silva Guia <guilhermeguia007@gmail.com>
2025-05-26 10:31:24 -0400

    Add DbContext in DI

--- src/Api/DI/DependencyInjectionExtension.cs
+++ src/Api/DI/DependencyInjectionExtension.cs
@@ -1,11 +1,28 @@
-﻿using Api.Services.Auth;
+﻿using Api.Infra.DataAccess;
+using Api.Services.Auth;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.DI;
 
 public static class DependencyInjectionExtension
 {
-    public static void AddServices(this IServiceCollection services)
+    public static void AddDI(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddServices(services);
+        AddDbContext(services, configuration);
+    }
+
+    public static void AddServices(IServiceCollection services)
     {
         services.AddScoped<IAuthService, AuthService>();
+    }
+    public static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Default");
+
+        services.AddDbContext<AutenticadorDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
     }
 }
