fd8c8d90b8860e3df3b741dfe193a23a4866308a
Guilherme da Silva Guia <guilhermeguia007@gmail.com>
2025-05-26 09:38:02 -0400

    Add repository

---
+++ src/Api/Infra/DataAccess/Repositories/Repository.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+using Api.Models.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Infra.DataAccess.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly AutenticadorDbContext _dbContext;
+    private readonly DbSet<T> _dbSet;
+    public Repository(AutenticadorDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _dbSet = dbContext.Set<T>();
+    }
+
+    public async Task Add(T entity) => await _dbSet.AddAsync(entity);
+    public void Delete(T entity) => _dbSet.Remove(entity);
+    public async Task<T>? Find(Expression<Func<T, bool>> predicate) => await _dbSet.Where(predicate).FirstOrDefaultAsync();
+    public async Task<IList<T>> GetAll() => await _dbSet.ToListAsync();
+    public async Task<T>? GetById(long Id) => await _dbSet.FindAsync(Id);
+    public void Update(T entity)
+    {
+        _dbContext.Entry(entity).State = EntityState.Modified;
+        _dbSet.Update(entity);
+    }
+}
---
+++ src/Api/Models/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Api.Models.Repositories;
+
+public interface IRepository<T> where T : class
+{
+    Task<IList<T>> GetAll();
+    Task<T?> Find(Expression<Func<T, bool>> predicate);
+    Task<T?> GetById(long Id);
+    Task Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
