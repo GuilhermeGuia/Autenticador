a3343a936fcc19273140ebe3589501cd4c38776a
Guilherme da Silva Guia <guilhermeguia007@gmail.com>
2025-05-26 08:47:01 -0400

    Add Dbcontext

---
+++ src/Api/Infra/DataAccess/AutenticadorDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Api.Infra.DataAccess;
+
+public class AutenticadorDbContext : DbContext
+{
+    public AutenticadorDbContext(DbContextOptions<AutenticadorDbContext> options) : base(options) { }
+    #region DBSETS
+        
+
+
+    #endregion
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AutenticadorDbContext).Assembly);
+    }
+}
